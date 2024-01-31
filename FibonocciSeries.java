705b0daa2acb60afe3f1142812a3498cedc5d3d8
01FE22BCA144 <156191963+Geronimo6986@users.noreply.github.com>
2024-01-31 13:29:22 +0530

    Code for Fibonocci Series

---
+++ FibonocciSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms for Fibonacci Series:");
+        int n = scanner.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+        System.out.print("Fibonacci Series: " + firstTerm + ", " + secondTerm);
+
+        for (int i = 2; i < n; i++) {
+            int nextTerm = firstTerm + secondTerm;
+            System.out.print(", " + nextTerm);
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
